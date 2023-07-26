ba33ae48405631fb0fc65a57b8ec758339ea70af
iyasud <iyasu.geleta@gmail.com>
2023-07-26 14:04:19 -0600

    fixing warnings

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -7,6 +7,7 @@ export const MovieDetail = () => {
   const params = useParams();
 
   const [movie, setMovie] = useState({});
+  //eslint-disable-next-line
   const pageTitle = useTitle(movie.title);
   const image = movie.poster_path
     ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
@@ -22,7 +23,7 @@ export const MovieDetail = () => {
       console.log(json);
     }
     fetchMovie();
-  }, []);
+  }, [params.id]);
 
   return (
     <main>
