5a557351fc4de7703bf963d5f303bd3f328077e5
iyasud <iyasu.geleta@gmail.com>
2023-11-25 10:13:57 -0700

    fixing enviromet varialbe issue

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -12,52 +12,18 @@ import {
 // export const useFetch = (apiPath, queryTerm = "") => {
 //   const [data, setData] = useState([]);
 
-//   const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`;
-//   useEffect(() => {
-//     async function fetchMovies() {
-//       const response = await fetch(url);
-//       const json = await response.json();
-//       setData(json.results);
-//     }
-//     fetchMovies();
-//   }, [url]);
-
-//   return { data };
-// };
 export const useFetch = (apiPath, queryTerm = "") => {
   const [data, setData] = useState([]);
 
-  const secret_name = "react_app_Secrete";
-
   useEffect(() => {
     async function fetchMovies() {
-      let response;
-      const client = new SecretsManagerClient({
-        region: "us-east-1",
-      });
-
-      try {
-        response = await client.send(
-          new GetSecretValueCommand({
-            SecretId: secret_name,
-            VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-          })
-        );
-      } catch (error) {
-        // For a list of exceptions thrown, see
-        // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        throw error;
-      }
-      console.log("secrete string==>", response.SecretString);
-      const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${response.SecretString}&query=${queryTerm}`;
+      const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`;
       const res = await fetch(url);
       const json = await res.json();
       setData(json.results);
     }
     fetchMovies();
   }, [apiPath, queryTerm]);
-
-  //   return { data };
 
   return { data };
 };
